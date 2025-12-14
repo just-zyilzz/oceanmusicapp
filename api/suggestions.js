a550c2d818775f79d2e1cf0d1d9e85375fa0269b
just-zyilzz <enzilaja@gmail.com>
2025-12-14 10:09:54 +0700

    fix: HTTP 103 timeout + enhanced youtube.js helpers
    - Fixed HTTP 103 timeout by using fast curated suggestions instead of slow youtubei.js API
    - Enhanced lib/youtube.js with comprehensive helpers (playlist, lyrics, artist, home feed, etc)
    - Kept real yt-search for search endpoint functionality

--- api/suggestions.js
+++ api/suggestions.js
@@ -1,5 +1,4 @@
-import { getYouTube } from '../lib/youtube.js';
-
+// Fast suggestions endpoint - returns known working songs instantly
 export default async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
@@ -7,24 +6,110 @@ export default async (req, res) => {
     if (req.method === 'OPTIONS') return res.status(200).end();
 
     try {
-        const yt = await getYouTube();
-
-        // Get "Quick Picks" or charts
-        // We'll search for a general term like "trending music" or "top hits" 
-        // to get a good list of songs since direct "home" feed might be personalized/empty without auth
-        const searchResult = await yt.music.search('top hits', { type: 'song' });
-
-        const suggestions = searchResult.results.map(song => ({
-            id: song.id,
-            videoId: song.id,
-            title: song.title,
-            artist: song.artists?.[0]?.name || 'Unknown',
-            album: song.album?.name || '',
-            duration: song.duration?.seconds * 1000 || 0,
-            thumbnail: song.thumbnails?.[0]?.url?.replace('w60-h60', 'w400-h400') || '',
-            artists: song.artists,
-            thumbnails: song.thumbnails
-        }));
+        // Use curated list of popular songs with known working IDs
+        // This is FAST and reliable (no external API calls)
+        const suggestions = [
+            {
+                id: 'dQw4w9WgXcQ',
+                videoId: 'dQw4w9WgXcQ',
+                title: 'Rick Astley - Never Gonna Give You Up',
+                artist: 'Rick Astley',
+                album: '',
+                duration: 213000,
+                thumbnail: 'https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg',
+                artists: [{ name: 'Rick Astley' }]
+            },
+            {
+                id: '9bZkp7q19f0',
+                videoId: '9bZkp7q19f0',
+                title: 'PSY - GANGNAM STYLE',
+                artist: 'PSY',
+                album: '',
+                duration: 252000,
+                thumbnail: 'https://i.ytimg.com/vi/9bZkp7q19f0/hqdefault.jpg',
+                artists: [{ name: 'PSY' }]
+            },
+            {
+                id: 'kJQP7kiw5Fk',
+                videoId: 'kJQP7kiw5Fk',
+                title: 'Luis Fonsi - Despacito ft. Daddy Yankee',
+                artist: 'Luis Fonsi',
+                album: '',
+                duration: 281000,
+                thumbnail: 'https://i.ytimg.com/vi/kJQP7kiw5Fk/hqdefault.jpg',
+                artists: [{ name: 'Luis Fonsi' }]
+            },
+            {
+                id: 'JGwWNGJdvx8',
+                videoId: 'JGwWNGJdvx8',
+                title: 'Ed Sheeran - Shape of You',
+                artist: 'Ed Sheeran',
+                album: '',
+                duration: 263000,
+                thumbnail: 'https://i.ytimg.com/vi/JGwWNGJdvx8/hqdefault.jpg',
+                artists: [{ name: 'Ed Sheeran' }]
+            },
+            {
+                id: 'OPf0YbXqDm0',
+                videoId: 'OPf0YbXqDm0',
+                title: 'Mark Ronson - Uptown Funk ft. Bruno Mars',
+                artist: 'Mark Ronson',
+                album: '',
+                duration: 270000,
+                thumbnail: 'https://i.ytimg.com/vi/OPf0YbXqDm0/hqdefault.jpg',
+                artists: [{ name: 'Mark Ronson' }]
+            },
+            {
+                id: 'CevxZvSJLk8',
+                videoId: 'CevxZvSJLk8',
+                title: 'Katy Perry - Roar',
+                artist: 'Katy Perry',
+                album: '',
+                duration: 223000,
+                thumbnail: 'https://i.ytimg.com/vi/CevxZvSJLk8/hqdefault.jpg',
+                artists: [{ name: 'Katy Perry' }]
+            },
+            {
+                id: 'fRh_vgS2dFE',
+                videoId: 'fRh_vgS2dFE',
+                title: 'Justin Bieber - Sorry',
+                artist: 'Justin Bieber',
+                album: '',
+                duration: 200000,
+                thumbnail: 'https://i.ytimg.com/vi/fRh_vgS2dFE/hqdefault.jpg',
+                artists: [{ name: 'Justin Bieber' }]
+            },
+            {
+                id: 'RgKAFK5djSk',
+                videoId: 'RgKAFK5djSk',
+                title: 'Wiz Khalifa - See You Again ft. Charlie Puth',
+                artist: 'Wiz Khalifa',
+                album: '',
+                duration: 229000,
+                thumbnail: 'https://i.ytimg.com/vi/RgKAFK5djSk/hqdefault.jpg',
+                artists: [{ name: 'Wiz Khalifa' }]
+            },
+            {
+                id: '2Vv-BfVoq4g',
+                videoId: '2Vv-BfVoq4g',
+                title: 'Ed Sheeran - Perfect',
+                artist: 'Ed Sheeran',
+                album: '',
+                duration: 263000,
+                thumbnail: 'https://i.ytimg.com/vi/2Vv-BfVoq4g/hqdefault.jpg',
+                artists: [{ name: 'Ed Sheeran' }]
+            },
+            {
+                id: 'ru0K8uYEZWw',
+                videoId: 'ru0K8uYEZWw',
+                title: 'The Chainsmokers - Closer ft. Halsey',
+                artist: 'The Chainsmokers',
+                album: '',
+                duration: 244000,
+                thumbnail: 'https://i.ytimg.com/vi/ru0K8uYEZWw/hqdefault.jpg',
+                artists: [{ name: 'The Chainsmokers' }]
+            }
+        ];
 
         res.json({
             success: true,
